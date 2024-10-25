d83ec6909095825939e8aadebd79ba1f8aac8f3f
glenn <glenn@gmail.com>
2024-10-25 23:36:55 +0800

    3rd commit

--- Feast/public/js/script.js
+++ Feast/public/js/script.js
@@ -2,3 +2,76 @@ function toggleDropdown() {
     var dropdown = document.getElementById("dropdown-menu");
     dropdown.classList.toggle("hidden");
 }
+
+const imgSlider = document.querySelector('.img-slider');
+const imgputahi = document.querySelectorAll('.img-item.putahi');
+const infoSlider = document.querySelector('.info-slider');
+const infobox = document.querySelector('.info-box');
+
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+let indexSlider = 0;
+let index = 0;
+let direction;
+
+nextBtn.addEventListener('click', () => {
+    indexSlider++;
+    imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+
+    index++;
+    if (index > imgputahi.length - 1) {
+        index = 0;
+    }
+
+    document.querySelector('.putahi.active').classList.remove('active');
+    imgputahi[index].classList.add('active');
+
+    if (direction === 1) {
+        infoSlider.prepend(infoSlider.lastElementChild);
+    }
+
+    direction = -1;
+    console.log(direction);
+
+    infobox.style.justifyContent = 'flex-start';
+    infoSlider.style.transform = 'translateY(-25%)';
+});
+
+prevBtn.addEventListener('click', () => {
+    indexSlider--;
+    imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+
+    index--;
+    if (index < 0) {
+        index = imgputahi.length - 1;
+    }
+
+    document.querySelector('.putahi.active').classList.remove('active');
+    imgputahi[index].classList.add('active');
+
+    if (direction === -1) {
+        infoSlider.appendChild(infoSlider.firstElementChild);
+    }
+
+    direction = 1;
+    console.log(direction);
+
+    infobox.style.justifyContent = 'flex-end';
+    infoSlider.style.transform = 'translateY(25%)';
+});
+
+infoSlider.addEventListener('transitionend', () => { 
+    if (direction === -1) {
+        infoSlider.appendChild(infoSlider.firstElementChild);
+    } else if (direction === 1) {
+        infoSlider.prepend(infoSlider.lastElementChild);
+    }
+
+    infoSlider.style.transition = 'none';
+    infoSlider.style.transform = 'translateY(0)';
+
+    setTimeout(() => {
+        infoSlider.style.transition = '.5s cubic-bezier(0.645, 0.045, 0.355, 1)';
+    });
+});
--- Feast/resources/views/welcome.blade.php
+++ Feast/resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Feast Downtown</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Anton+SC&display=swap');
         body {
@@ -18,7 +20,7 @@
     <nav class="flex justify-between items-center p-4 ">
         <img class="w-32" src="/images/logo.png" alt="Feast Downtown">
         
-        <nav-list class="flex items-center relative"> <!-- Added relative positioning here -->
+        <nav-list class="flex items-center relative"> 
             <div>
                 <svg class="w-10 hidden sm:block" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0L0 6V8H1V15H4V10H7V15H15V8H16V6L14 4.5V1H11V2.25L8 0ZM9 10H12V13H9V10Z" fill="#000000"></path> </g></svg>
             </div>
@@ -46,30 +48,73 @@
         </nav-list>
     </nav>
 </header>
-    <main>
-        <div>
-            <div class="show-top mt-6 grid lg:flex lg:h-[70vh] lg:m-auto">
-                    <div class="socialmedia flex justify-center items-center gap-2 mb-4 lg:grid lg:h-20 lg:m-auto ">
-                        <svg class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"/></svg>
-                        
-                        <svg class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7 53.3 53.3 0 1 0 -59.3-88.7zm142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9 .1-11.2 .1c-3.3 0-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1 .1 7.9 .1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1c3.3 0 7.2 0 11.4 .1c25.5 .3 64.8 .7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1 .1-11.4c.3-25.5 .7-64.9-6.5-83l0 0c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5A82 82 0 1 1 178.4 324.2a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5l0 0c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM357 389c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z"/></svg>
 
-                        <svg class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM351.3 199.3v0c0 86.7-66 186.6-186.6 186.6c-37.2 0-71.7-10.8-100.7-29.4c5.3 .6 10.4 .8 15.8 .8c30.7 0 58.9-10.4 81.4-28c-28.8-.6-53-19.5-61.3-45.5c10.1 1.5 19.2 1.5 29.6-1.2c-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3c-9-6-16.4-14.1-21.5-23.6s-7.8-20.2-7.7-31c0-12.2 3.2-23.4 8.9-33.1c32.3 39.8 80.8 65.8 135.2 68.6c-9.3-44.5 24-80.6 64-80.6c18.9 0 35.9 7.9 47.9 20.7c14.8-2.8 29-8.3 41.6-15.8c-4.9 15.2-15.2 28-28.8 36.1c13.2-1.4 26-5.1 37.8-10.2c-8.9 13.1-20.1 24.7-32.9 34c.2 2.8 .2 5.7 .2 8.5z"/></svg>
-                    </div>
-                    <div class="products grid lg:flex lg:flex-row lg:items-center lg:justify-center ml-10 ">
-                        <div class="product-name text-center">
-                            <h1 class="text-[#C7253E] font-bold uppercase text-4xl sm:text-6xl lg:basis-2/6">burger</h1>
-                        </div>
-                        <div class="product-image grid items-center justify-center">
-                            <img src="/images/burger.png" alt="burger image" class="lg:basis-2/5">
-                        </div>
-                        <div class="product-description text-[#C7253E] text-center  lg:basis-2/6">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sapiente nulla numquam facere repudiandae mollitia. Nemo odit quae deleniti debitis dignissimos officia, deserunt ullam, corrupti saepe et architecto id corporis.
-                        </div>
-                    </div>
-                </div>
+<main>
+    <div class="carousel">
+    <div class="img-box">
+        <div class="img-list">
+        <div class="img-slider">
+            <div class="img-item putahi active" style="--i:0;">
+                <img src="/images/burgerbg.png" alt="burgerbg.png">
+            </div>
+            <div class="img-item putahi" style="--i:1;">
+                <img class="w-[500px]" src="/images/burgerbg.png" alt="pizza 2.png">
+            </div>
+            <div class="img-item putahi" style="--i:2;">
+                <img  class="w-[500px]" src="/images/burgerbg.png" alt="friedchicken.png">
+            </div>
+            <div class="img-item putahi" style="--i:3;">
+                <img  class="w-[500px]"  src="/images/burgerbg.png" alt="lasagna.png">
+            </div> 
 
-                <div class="show-food mt-6">
+            <div class="img-item" style="--i:0;">
+                <img src="/images/burger.png" alt="burger.png">
+            </div>
+            <div class="img-item " style="--i:1;">
+                <img class="w-[500px]" src="/images/pizza 2.png" alt="pizza 2.png">
+            </div>
+            <div class="img-item" style="--i:2;">
+                <img  class="w-[500px]" src="/images/friedchicken.png" alt="friedchicken.png">
+            </div>
+            <div class="img-item" style="--i:3;">
+                <img  class="w-[500px]"  src="/images/lasagna.png" alt="lasagna.png">
+            </div> 
+        </div>
+        </div>
+    </div>
+
+    <div class="info-box">
+        <div class="info-slider">
+            <div class="info-item">
+                <h2 class="text-[#C7253E]">burger</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sed atque facere molestias modi voluptates nemo aliquid deleniti numquam quos.</p>
+            </div>
+            <div class="info-item">
+                <h2 class="text-[#C7253E]">pizza</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sed atque facere molestias modi voluptates nemo aliquid deleniti numquam quos.</p>
+            </div>
+            <div class="info-item">
+                <h2 class="text-[#C7253E]">friedchicken</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sed atque facere molestias modi voluptates nemo aliquid deleniti numquam quos.</p>
+            </div>
+            <div class="info-item">
+                <h2 class="text-[#C7253E]">lasagna</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores sed atque facere molestias modi voluptates nemo aliquid deleniti numquam quos.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="large-text">
+        <h2>Feast</h2>
+
+    </div>
+
+    <div class="navigation">
+        <span class="prev-btn"><i class='bx bx-chevron-left'></i></span>
+        <span class="next-btn"><i class='bx bx-chevron-right' ></i></span>
+    </div>
+    </div>
+    <div class="show-food mt-6">
                     <div class="product-list grid grid-cols-2 sm:grid sm:grid-cols-3 lg:grid lg:grid-cols-4 grid-flow-row ">
                         <div class="bg-[#FFC82A] flex items-center justify-center m-2">
                             <img class="w-full h-full object-contain" src="/images/burger.png" alt="burger">
@@ -100,8 +145,8 @@
 
                 
 
-                <div class="special-offer mt-6 grid">
-                    <div class="produc-special-offer lg:flex">
+                <div class="special-offer mt-6 flex h-[50vh]">
+                    <div class="produc-special-offer lg:flex  m-auto">
                         <div class="product-special-offer-text text-center lg:m-auto basis-1/2">
                             <h1 class="text-4xl">special offer <br/><span class="text-[#C7253E] ">50% off</span></h1>
                             <p class="mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, quisquam!</p> 
@@ -138,8 +183,12 @@
                 </div>
             </div>
         </div>
-    </main>
-    <footer>
+
+    
+
+</main>
+
+<footer>
     <div class="grid grid-cols-2 grid-rows-2 gap-10 mt-10 text-center lg:flex lg:mt-20">
         <div class="popular-page-list flex-1">
             <div>
@@ -198,6 +247,9 @@
         </div>
     </div>
 </footer>
+
+
+
 
 <script src="{{ asset('js/script.js') }}"></script>
 
